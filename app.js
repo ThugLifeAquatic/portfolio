dc2480dc0e4d75bb3128386c09ad30b9f7d3e813
Austin Cohrs <acohrsdev@outlook.com>
2017-07-11 11:41:06 -0700

    Forgot my js Constructor!

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function ProjectModel(name, description, image, link ){
+  this.name = name;
+  this.description = description;
+  this.image = image;
+  this.link =link;
+  ProjectModel.all.push(this);
+}
+
+ProjectModel.all = [];
