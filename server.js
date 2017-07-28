1d0b208df8dda718c12bda95963625a73d80006b
Austin Cohrs <acohrsdev@outlook.com>
2017-07-28 11:49:09 -0700

    ready for heroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ function proxyGitHub(request, response) {
   console.log('Routing GitHub request for', request.params[0]);
   (requestProxy({
     url: `https://api.github.com/${request.params[0]}`,
-    headers: {Authorization: `TOKEN GOES HERE :)`}
+    headers: {Authorization: `token ${GITHUB_TOKEN}`}
   }))(request, response);
 }
 
