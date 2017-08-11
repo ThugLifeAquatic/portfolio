42aa608509f6b0c9c163bb32b29b1ebb1afd1f66
Austin Cohrs <acohrsdev@outlook.com>
2017-08-11 13:20:30 -0700

    final touches.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
